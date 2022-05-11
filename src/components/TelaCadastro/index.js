cbaf217fa36d6c36971981a49352e3aeb7504bea
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-05-11 17:45:24 -0300

    feat: input controlado login/cadastro

--- src/components/TelaCadastro/index.js
+++ src/components/TelaCadastro/index.js
@@ -1,25 +1,42 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import Botao from '../utils/Botao.js';
+import Paragrafo from '../utils/Paragrafo.js';
 
 import { Container } from "./style";
 
 function TelaCadastro() {
     const arrayInputs = ['Nome', 'E-mail', 'Senha', 'Confirme a senha']
+    const [dadosCadastro, setDadosCadastro] = useState({
+        name: '', email: '', password: '', confirmPassword: ''
+    });
+
+    console.log(dadosCadastro);
+
+    async function postDadosCadastro(e) {
+        e.preventDefault();
+    }
 
     return ( 
         <Container>
-            <form>
+            <form onSubmit={postDadosCadastro}>
                 <div className="inputs">
-                    <input type="text" placeholder={arrayInputs[0]} />
-                    <input type="email" placeholder={arrayInputs[1]} />
-                    <input type="password" placeholder={arrayInputs[2]} />
-                    <input type="password" placeholder={arrayInputs[3]} />
+                    <input type="text" placeholder={arrayInputs[0]} value={dadosCadastro.name} required
+                    onChange={(e)=>setDadosCadastro({...dadosCadastro, name: e.target.value})}/>
+                    <input type="email" placeholder={arrayInputs[1]} value={dadosCadastro.email} required
+                    onChange={(e)=>setDadosCadastro({...dadosCadastro, email: e.target.value})}/>
+                    <input type="password" placeholder={arrayInputs[2]} value={dadosCadastro.password} required
+                    onChange={(e)=>setDadosCadastro({...dadosCadastro, password: e.target.value})}/>
+                    <input type="password" placeholder={arrayInputs[3]} value={dadosCadastro.confirmPassword} required
+                    onChange={(e)=>setDadosCadastro({...dadosCadastro, confirmPassword: e.target.value})}/>
                 </div>
                 <div className="botao">
-                    <button type="submit">Cadastrar</button>
+                    <Botao tipo="submit" conteudo="Cadastrar" />
                 </div>
             </form>
             <Link to="/signin">
-                <p>Já possui conta? Faça login agora!</p>
+                <Paragrafo conteudo="Já possui conta? Faça login agora!" />
             </Link>
         </Container>
     );
--- src/components/TelaLogin/index.js
+++ src/components/TelaLogin/index.js
@@ -1,23 +1,38 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import Paragrafo from '../utils/Paragrafo.js';
+import Botao from '../utils/Botao.js';
 
 import { Container } from "../TelaCadastro/style.js";
 
 function TelaLogin() {
     const arrayInputs = ['E-mail', 'Senha'];
+    const [dadosLogin, setDadosLogin] = useState({
+        email: '', password: ''
+    });
+
+    console.log(dadosLogin);
+
+    async function postDadosLogin(e) {
+        e.preventDefault();
+    }
 
     return ( 
         <Container>
-            <form>
+            <form onSubmit={postDadosLogin}>
                 <div className="inputs">
-                    <input type="email" placeholder={arrayInputs[0]} />
-                    <input type="password" placeholder={arrayInputs[1]} />
+                    <input type="email" placeholder={arrayInputs[0]} value={dadosLogin.email} required 
+                    onChange={(e)=>setDadosLogin({...dadosLogin, email: e.target.value})} />
+                    <input type="password" placeholder={arrayInputs[1]} value={dadosLogin.password} required 
+                    onChange={(e)=>setDadosLogin({...dadosLogin, password: e.target.value})} />
                 </div>
                 <div className="botao">
-                    <button type="submit">Entrar</button>
+                    <Botao tipo="submit" conteudo="Entrar" />
                 </div>
             </form>
             <Link to="/signup">
-                <p>Não possui conta? Faça seu cadastro!</p>
+                <Paragrafo conteudo="Não possui conta? Faça seu cadastro!" />
             </Link>
         </Container>
      );
