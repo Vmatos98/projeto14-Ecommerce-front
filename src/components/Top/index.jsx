d573ca12ce6383c8f5e29ddf763611277ac558c9
Victor Matos <antoniovitomatos@gmail.com>
2022-05-12 12:49:05 -0300

    leyout do topo e quantidade de itens no carrinho

--- src/components/Top/index.jsx
+++ src/components/Top/index.jsx
@@ -1,10 +1,33 @@
-import { useState} from "react";
+import { useState, useEffect} from "react";
+import axios from "axios";
 
 import {Header, Search, User} from "./../Style/Top";
 import Logo from "../../Assets/img/logoPrincipal.jpg";
 function Top(){
-    const [search, setSearch] = useState("");
-    console.log(search);
+    const URL = 'http://localhost:5000';
+    const cartId = localStorage.getItem("cartId");
+    const [search, setSearch] = useState('');
+    const [amount, setAmount] = useState('');
+    useEffect(() => {
+        
+            const createCart = async() =>{
+                const response = await axios.post(`${URL}/cart/create`);
+                localStorage.setItem("cartId", response.data);
+            }
+            const getAmount = async() =>{
+                const response = await axios.get(`${URL}/cart/amount/${cartId}`);
+                setAmount(response.data);
+            }
+            if(!cartId){
+                createCart();
+                document.location.reload(true);
+            }
+
+            getAmount();
+    
+    
+    }, []);
+    
     return (
         <Header>
             <img src={Logo} alt="Logo" />
@@ -15,7 +38,7 @@ function Top(){
             <User>
                 <div>
                 <ion-icon name="cart-outline"></ion-icon>
-                <p>0</p>
+                <p>{amount}</p>
                 </div>
                 <div>
                 <ion-icon name="person-circle-outline"></ion-icon>
